5aa8d22129c1569aa5247daf1289f8e0daf63de2
olha-kudla <119434247+olha-kudla@users.noreply.github.com>
2023-05-08 14:58:29 +0300

    task #7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const imgCard = createGalleryMurkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", imgCard);
+
+galleryEl.addEventListener('click', onImageClick)
+
+function createGalleryMurkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+})
+.join('');
+}
+
+function onImageClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(evt.target)
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+    `)
+
+    instance.show()
+
+    document.addEventListener('keydown', onEscapePress)
+
+    function onEscapePress(evt) {
+        if (evt.code === 'Escape') {
+            instance.close();
+            document.removeEventListener('keydown', onEscapePress);
+        }
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const imgCard = createGalleryMurkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", imgCard);
+
+galleryEl.addEventListener('click', onImageClick)
+
+function createGalleryMurkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`;
+})
+.join('');
+}
+
+function onImageClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(evt.target)
+}
+
+const lightbox = new SimpleLightbox('.gallery__link',
+{ captionsData: "alt", captionDelay: "250" })
